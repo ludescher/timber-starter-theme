37f98d9367b2d3b0b25076f2e8b2518c9edc1bca
Johannes Ludescher <ludescher.j@gmail.com>
2019-07-15 09:17:17 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,3 @@
 require('../scss/app.scss');
 
-let link = document.getElementsByClassName("js-get-link")[0].getAttribute("data-link");
-
-console.log("Call Link: ", link);
-
-fetch(link, {
-    method: 'POST',
-    headers: {
-        'accept': 'application/json',
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        email: "franz@josef.com",
-        password: "suckers"
-    }),
-})
-.then((response) => {
-    return response.json();
-})
-.then((myJson) => {
-    console.log(myJson);
-})
-.catch((error) => {
-    console.error(error);
-});
+console.log('Hello Webpack Encore');
